4f817242e88bd58943f2179b999f8e8f3691edb4
uma victor <umavictor11@email.com>
2025-06-13 17:15:34 +0100

    add node health script

---
+++ nodehealth.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+####################
+# Author: Uma Victor
+# Date: 13/06/2025
+#
+#
+# This script outputs the node health
+#
+#
+# Version: v1
+####################
+
+
+set -x ## Debug mode
+
+df -h # check disk
+
+free -g # check memory
+
+nproc # check resources
+
+ps -ef | grep amazon
+
+ps -ef | grep amazon | awk -F" " '{print $2}'
